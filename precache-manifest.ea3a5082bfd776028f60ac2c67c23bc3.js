c9caa51ceb134e783891cc50eccd8a5c2e55c49b
MikaCinc <mihajlo.ls00@outlook.com>
2020-07-21 16:15:48 +0200

    Updates

---
+++ precache-manifest.ea3a5082bfd776028f60ac2c67c23bc3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da59f25949c999fcefc95f253d0ac52c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "16ea941549f7698469d9",
+    "url": "/portfolio/static/css/main.d83df2cb.chunk.css"
+  },
+  {
+    "revision": "263266cfa36dadd01b85",
+    "url": "/portfolio/static/js/2.0d1117c7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.0d1117c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16ea941549f7698469d9",
+    "url": "/portfolio/static/js/main.03005bc1.chunk.js"
+  },
+  {
+    "revision": "361b25be0752ed4bc489",
+    "url": "/portfolio/static/js/runtime-main.b679d28b.js"
+  },
+  {
+    "revision": "ca8c18a89ec14ecbfc84f4443791acdc",
+    "url": "/portfolio/static/media/bio_pic.ca8c18a8.jpg"
+  },
+  {
+    "revision": "e1c6f5ac422ea2c545a57fd56416379b",
+    "url": "/portfolio/static/media/durango.e1c6f5ac.png"
+  },
+  {
+    "revision": "9577f6bf14f9e8bcf1835b254256c6d1",
+    "url": "/portfolio/static/media/prof.9577f6bf.jpg"
+  },
+  {
+    "revision": "a90e7c681b848ca35f57e9181548dae1",
+    "url": "/portfolio/static/media/supreme.a90e7c68.jpg"
+  }
+]);
