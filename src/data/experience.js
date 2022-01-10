3175e7a5e24985e21b066bac5c4613615dac567b
MikaCinc <mihajlo.ls00@outlook.com>
2022-01-10 15:46:48 +0100

    Updated skills

--- src/data/experience.js
+++ src/data/experience.js
@@ -1,94 +1,100 @@
 const experience = [
-    {
-        title: 'Web Developer @ Boopro',
-        date: 'October 2020 - present',
-        priority: 'main'
-    },
-    {
-        title: 'Web Developer @ FutureForward',
-        date: 'December 2017 - October 2020',
-        priority: 'main'
-    },
-    {
-        title: 'Co-Founder & Lead developer @ Durango',
-        date: 'December 2019 - September 2020',
-        priority: 'main'
-    },
-    {
-        title: 'Microsoft Windows Insider',
-        date: '2015 - present',
-        priority: 'other'
-    },
-    {
-        title: 'Coordinator & chief presenter @ Project ECHO',
-        date: 'May 2019',
-        priority: 'main'
-    },
-    {
-        title: 'Public speaker @ national project #BudućnostZa5',
-        date: 'April 2019',
-        priority: 'main'
-    },
-    {
-        title: '(particip.) IT Konekt',
-        date: 'April 2019',
-        priority: 'other'
-    },
-    {
-        title: 'Photoshoping glasses for Clair Optiek',
-        date: 'Jun 2018 - March 2019',
-        priority: 'main'
-    },
-    {
-        title: 'Developer @ Fitness Supreme',
-        date: 'August 2018 - November 2018',
-        priority: 'main'
-    },
-    {
-        title: 'Founder & Mentor @ //KODIAK/',
-        date: 'November 2017 - April 2018',
-        priority: 'main'
-    },
-    {
-        title: "Member @ NGO 'Nauči Me'",
-        date: 'January 2017 - September 2018',
-        priority: 'main'
-    },
-    {
-        title: 'Mentor @ CodeRobotic(V. 1.0 & V. 2.0)',
-        date: 'January 2017 - December 2018',
-        priority: 'other'
-    },
-    {
-        title: "PR Assistant & Booth Presenter @ Festival 'Nauk Nije Bauk' (8, 9, 10)",
-        date: 'March - April / 2016, 2017, 2018',
-        priority: 'other'
-    },
-    {
-        title: "Volunteer @ Festival 'Night Of Biology'",
-        date: 'May 2017 / May 2018',
-        priority: 'other'
-    },
-    {
-        title: '(particip.) Best Week seminar',
-        date: 'April 2017',
-        priority: 'other'
-    },
-    {
-        title: '(particip.) Professional orientation seminar',
-        date: 'June 2017',
-        priority: 'other'
-    },
-    {
-        title: '(particip.) Nature conservation & Sports camp',
-        date: 'July 2017',
-        priority: 'other'
-    },
-    {
-        title: 'Assistant @ Camp of the physics & science',
-        date: 'January / 2013 - 2014',
-        priority: 'other'
-    }
+  {
+    title: "Web Developer @ Boopro",
+    date: "October 2020 - present",
+    priority: "main",
+  },
+  {
+    title: "React Native developer @ Attulo",
+    date: "September 2021 - December 2021",
+    priority: "main",
+  },
+  {
+    title: "Web Developer @ FutureForward",
+    date: "December 2017 - October 2020",
+    priority: "main",
+  },
+  {
+    title: "Co-Founder & Lead developer @ Durango",
+    date: "December 2019 - September 2020",
+    priority: "main",
+  },
+  {
+    title: "Coordinator & chief presenter @ Project ECHO",
+    date: "May 2019",
+    priority: "main",
+  },
+  {
+    title: "Public speaker @ national project #BudućnostZa5",
+    date: "April 2019",
+    priority: "main",
+  },
+  {
+    title: "(particip.) IT Konekt",
+    date: "April 2019",
+    priority: "other",
+  },
+  {
+    title: "Photoshoping glasses for Clair Optiek",
+    date: "Jun 2018 - March 2019",
+    priority: "main",
+  },
+  {
+    title: "Developer @ Fitness Supreme",
+    date: "August 2018 - November 2018",
+    priority: "main",
+  },
+  {
+    title: "Founder & Mentor @ //KODIAK/",
+    date: "November 2017 - April 2018",
+    priority: "main",
+  },
+  {
+    title: "Member @ NGO 'Nauči Me'",
+    date: "January 2017 - September 2018",
+    priority: "main",
+  },
+  {
+    title: "Mentor @ CodeRobotic(V. 1.0 & V. 2.0)",
+    date: "January 2017 - December 2018",
+    priority: "other",
+  },
+  {
+    title: "Microsoft Windows Insider",
+    date: "2015 - present",
+    priority: "other",
+  },
+  {
+    title:
+      "PR Assistant & Booth Presenter @ Festival 'Nauk Nije Bauk' (8, 9, 10)",
+    date: "March - April / 2016, 2017, 2018",
+    priority: "other",
+  },
+  {
+    title: "Volunteer @ Festival 'Night Of Biology'",
+    date: "May 2017 / May 2018",
+    priority: "other",
+  },
+  {
+    title: "(particip.) Best Week seminar",
+    date: "April 2017",
+    priority: "other",
+  },
+  {
+    title: "(particip.) Professional orientation seminar",
+    date: "June 2017",
+    priority: "other",
+  },
+  {
+    title: "(particip.) Nature conservation & Sports camp",
+    date: "July 2017",
+    priority: "other",
+  },
+  {
+    title: "Assistant @ Camp of the physics & science",
+    date: "January / 2013 - 2014",
+    priority: "other",
+  },
 ];
 
 export default experience;
